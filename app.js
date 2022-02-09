83cb8797322d47e02073f4843b5d33222539f036
ashleywebb <ashleywebb112@gmail.com>
2022-02-08 17:32:27 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// // Activity 1
+
+// let a = '';
+// let b = 'bongos';
+// let c = 'get your';
+
+// a = b; // bongos
+// b = c; // get your
+// c = a; // bongos
+
+
+// // Activity 2
+
+// console.log("Please " + "squeeze " + "the " + " cheese")
+// const word = 'believe'
+// console.log("please " + word + " the cheese.")
+// five = 5;
+// ten = 10;
+// console.log("The sum of " + five + " and " + "ten is " + (ten + 5))
+
+
+// // Activity 3
+
+// console.log(-10 >= -100)
+// console.log(0 < 1000)
+// console.log(888 === 889)
+// console.log(20 <= 20)
+
+
+// //Activity 4
+// let i = 0
+// while (i < 100) {
+//     i++;
+//     console.log(i)
+// }
+
+// let j = 0
+// while (j < 5000) {
+//     j++;
+//     console.log(j * j)
+// }
+
+// let a = 1000;
+// while(a > 0) {
+//     console.log(a)
+//     a--;
+// }
+
+// Activity 5
+
+// for (b = 0; b < 10000; b++) {
+//     console.log(b)
+// }
+
+for (c = 0; c <= 100; c++) {
+    console.log('This is the song that never ends')
+    console.log(c)
+}
+
+// for (d = 1000; d > 0; d++){
+//     console.log(i)
+// }
